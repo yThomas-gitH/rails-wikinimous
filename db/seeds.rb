aa692666e4650149ff106135dcb9ea5c0c182b1b
Yann Thomas <yannthomas1995@gmail.com>
2025-05-27 16:14:09 +0200

    challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do 
+   new_article = Article.new(title: Faker::Game.title, content: Faker::Quote.famous_last_words)
+   new_article.save
+end
